e7280d60c591ccc7773ec00084cc897d52b2b201
lilicrst <lisandra.crisilva37@gmail.com>
2022-06-27 18:23:02 -0300

    project created

---
+++ praticas/01_1-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,16 @@
+import { Post } from "./Post";
+
+export function App() {
+  return (
+    <>
+    <Post 
+    author="Lisandra" 
+    content="Texto qualquer"
+    />
+    <Post 
+    author="Ferdinando" 
+    content="Texto qualquer só que de ferdi"
+    />
+    </>
+  )
+}
