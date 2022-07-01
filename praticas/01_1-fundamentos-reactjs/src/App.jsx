3efa0d8f2aaddf3317793e20c503cdc8c00d6c2e
lilicrst <lisandra.crisilva37@gmail.com>
2022-07-01 15:55:16 -0300

    css sidebar finished

--- praticas/01_1-fundamentos-reactjs/src/App.jsx
+++ praticas/01_1-fundamentos-reactjs/src/App.jsx
@@ -1,21 +1,27 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
 
-import './global.css'
+import styles from './App.module.css';
+import './global.css';
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <>
       <Header />
-
-      <Post
-        author="Lisandra"
-        content="Texto qualquer"
-      />
-      <Post
-        author="Ferdinando"
-        content="Texto qualquer só que de ferdi"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Lisandra"
+            content="Texto qualquer"
+          />
+          <Post
+            author="Ferdinando"
+            content="Texto qualquer só que de ferdi"
+          />
+        </main>
+      </div>
     </>
-  )
+  );
 }
